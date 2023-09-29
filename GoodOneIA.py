3d8950ff5cd5332c8b6f720552b3177e0f96a1f4
ElizabethS <setharth1998@gmail.com>
2023-09-29 20:28:46 +0200

    initialisation

---
+++ GoodOneIA.py
@@ -0,0 +1,32 @@
+from moviepy.editor import *
+import os
+IMAGEMAGICK_BINARY = "C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\convert.exe"
+os.environ["IMAGEIO_FFMPEG_EXE"] = "C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\convert.exe"
+
+from moviepy.editor import *
+
+# Загрузите фоновое изображение
+background_image = ImageClip("Liam.jpeg")
+
+# Определите текст и создайте список слов
+text = "Il était une fois, dans un petit village au cœur d'une vallée verdoyante, un jeune garçon nommé Liam. Liam était un rêveur, toujours plongé dans les pages de ses livres préférés."
+words = text.split()  # Разделите текст на слова
+
+
+video_clips = []
+current_time = 0  
+
+for word in words:
+    word_clip = TextClip(word, fontsize=30, color="white")
+    word_duration = len(word) * 0.1  
+    word_clip = word_clip.set_duration(word_duration)
+    word_clip = word_clip.set_position("center")
+    word_clip = word_clip.set_start(current_time)
+    video_clips.append(word_clip)
+    current_time += word_duration
+
+
+final_video = CompositeVideoClip([background_image.set_duration(current_time)] + video_clips)
+
+
+final_video.write_videofile("output_video.mp4", fps=24)
